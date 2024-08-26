fedf70944a487437c4de539f274f9288f4455e50
Maxim Masiutin <maxim@masiutin.com>
2024-08-26 07:14:00 +0300

    Improved path handling; also looking for a newer powershell executable if exists

--- stop-services.cmd
+++ stop-services.cmd
@@ -1,2 +1,13 @@
 #echo You should run this cript from the current directory
-powershell -file %~dp0%stop-services.ps1
+
+for %%X in (pwsh.exe) do (set FOUND=%%~$PATH:X)
+echo %FOUND%
+
+if defined FOUND (
+  set POWERSHELLEXECUTABLE="%FOUND%"
+) else (
+  set POWERSHELLEXECUTABLE="powershell.exe"
+)
+
+echo %POWERSHELLEXECUTABLE%
+%POWERSHELLEXECUTABLE% -file "%~dp0%stop-services.ps1"

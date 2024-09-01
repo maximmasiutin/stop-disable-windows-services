16321cfca9ec577dc521aa0a537edfd3acc24414
Maxim Masiutin <maxim@masiutin.com>
2024-09-01 03:21:29 +0300

    WIP

--- stop-services.cmd
+++ stop-services.cmd
@@ -1,7 +1,7 @@
-#echo You should run this cript from the current directory
+@echo off
+rem You should run this cript from the current directory
 
 for %%X in (pwsh.exe) do (set FOUND=%%~$PATH:X)
-echo %FOUND%
 
 if defined FOUND (
   set POWERSHELLEXECUTABLE="%FOUND%"
@@ -9,5 +9,4 @@ if defined FOUND (
   set POWERSHELLEXECUTABLE="powershell.exe"
 )
 
-echo %POWERSHELLEXECUTABLE%
 %POWERSHELLEXECUTABLE% -file "%~dp0%stop-services.ps1"
